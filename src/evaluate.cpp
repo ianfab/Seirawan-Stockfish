77a4a5b79c2ee1bf9e87020e711ebbb47466c376
hxim <hxim.jo@gmail.com>
2018-01-04 22:31:30 +0100

    Remove ThreatByHangingPawn bonus (#1356)
    * no ThreatByHangingPawn
    
    * bench: 4919682
    
    Conflicts:
    
    	src/evaluate.cpp

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -237,7 +237,6 @@ namespace {
   const Score OtherCheck          = S( 10, 10);
   const Score CloseEnemies        = S(  7,  0);
   const Score PawnlessFlank       = S( 20, 80);
-  const Score ThreatByHangingPawn = S( 71, 61);
   const Score ThreatBySafePawn    = S(192,175);
   const Score ThreatByRank        = S( 16,  3);
   const Score Hanging             = S( 48, 27);
@@ -571,9 +570,6 @@ namespace {
         safeThreats = (shift<Right>(b) | shift<Left>(b)) & weak;
 
         score += ThreatBySafePawn * popcount(safeThreats);
-
-        if (weak ^ safeThreats)
-            score += ThreatByHangingPawn;
     }
 
     // Squares strongly protected by the opponent, either because they attack the
