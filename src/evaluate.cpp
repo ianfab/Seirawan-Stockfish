7d1cf1790b14c9a8577249271d813ba14c42dac3
ceebo <chricainogithub@gmail.com>
2018-02-03 15:35:38 +0000

    Include x-ray attacks through (most) super-pieces
    Seirawan implementation of
    
    https://github.com/official-stockfish/Stockfish/commit/7d4d3a2c3a4c6ce96bdbcfecf373934fdccc9b07
    
    I could have also added Rook x-rays through opponents Hawk but decided
    the line was long enough already.
    
    Bench: 1672447

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -333,8 +333,8 @@ namespace {
     while ((s = *pl++) != SQ_NONE)
     {
         // Find attacked squares, including x-ray attacks for bishops and rooks
-        b = Pt == BISHOP ? attacks_bb<BISHOP>(s, pos.pieces() ^ pos.pieces(Us, HAWK, QUEEN))
-          : Pt ==   ROOK ? attacks_bb<  ROOK>(s, pos.pieces() ^ pos.pieces(Us, ROOK, ELEPHANT, QUEEN))
+        b = Pt == BISHOP ? attacks_bb<BISHOP>(s, pos.pieces() ^ pos.pieces(HAWK, QUEEN) ^ pos.pieces(Them, ELEPHANT))
+          : Pt ==   ROOK ? attacks_bb<  ROOK>(s, pos.pieces() ^ pos.pieces(ELEPHANT, QUEEN) ^ pos.pieces(Us, ROOK))
                          : pos.attacks_from<Pt>(s);
 
         if (pos.pinned_pieces(Us) & s)
